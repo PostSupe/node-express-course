2b1dcce3c14f7676acb6a3ffb8b33a5a5e770251
Anthony Pratt <anthonypratt@GSV-Sleeper-Service-4.local>
2021-06-25 13:26:30 +1200

    added dynamic GET route from URL

--- server.js
+++ server.js
@@ -17,6 +17,17 @@ app.get('/users', function(req,res){
 })
 
 
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user (from the URL request)',
+		user: req.params.id
+	})
+})
+
+
+
 app.listen(8000,function(){
 console.log("server is running - listening on Port 8000")
 })
