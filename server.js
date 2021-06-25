3c7ef5f93d978e46987f108412867f5eaf99e990
Anthony Pratt <anthonypratt@GSV-Sleeper-Service-4.local>
2021-06-25 13:19:50 +1200

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 
+
+
+const mockUserData=[
+{name:'Logan'},
+{name:'Joshua'},
+{name:'Amy-Lee'}
+]
+app.get('/users', function(req,res){
+ 	res.json({
+ 	 	success: true,
+ 	 	message: 'woot -- successfully got users. Nice!',
+ 	 	users: mockUserData
+ 	})
+})
+
+
 app.listen(8000,function(){
 console.log("server is running - listening on Port 8000")
 })
